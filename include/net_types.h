256e661277c0e205c944b52901c3267137423771
Aleksandr Yurev <n/a>
2014-07-18 12:00:15 +0400

    Change net_types.h 	Add headers for net 	Add prototype message 	Add types message

--- include/net_types.h
+++ include/net_types.h
@@ -1,6 +1,41 @@
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 smartindent: */
 /*
+ * ip - loopback
+ * port -  1234
+ *
  * net_types.h
- * net_type_message
- * net_prototype
+ *
+ * net_prototype msg
+ *		type NICK
+ *			1 byte - type message
+ *			2 byte - nickname
+ *		type PLACEMENT
+ *			1 byte - type message
+ *			2 byte - placement
+ *		type START
+ *			1 byte - type message
+ *			2 byte - turn
+ *		type SHOT
+ *			1 byte - type message
+ *			2 byte - value x
+ *			3 byte - value y
+ *			4 byte - result of shot
+ *		type ERROR
+ *			1 byte - type message
+ *			2 byte - error code
  */
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/poll.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+enum types_msg{
+	NICK,
+	PLACEMENT,
+	START,
+	SHOT,
+	ERROR
+};
