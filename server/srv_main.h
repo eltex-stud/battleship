63782c61672f7e1731c8f6b1ac3f485b42b5afe0
zaquest <web.elektro.net@gmail.com>
2014-07-18 14:45:48 +0700

    Server main header and server net client operations

--- server/srv_main.h
+++ server/srv_main.h
@@ -1,1 +1,36 @@
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 smartindent: */
+#ifndef BATTLESHIP_SRV_MAIN_H_
+#define BATTLESHIP_SRV_MAIN_H_
+
+#include <server/srv_net.h>
+
+enum srv_main_turn { MY, ENEMY };
+
+struct srv_main_client_data {
+	struct srv_main_client_data *enemy;
+	struct srv_net_client *client;
+	struct srv_logic_map *map;
+	enum srv_main_turn turn;
+};
+
+struct srv_main_data_list {
+	struct srv_main_client_data *client_data;
+	struct srv_main_data_list *next;
+
+};
+
+struct srv_main_data {
+	struct srv_main_data_list *clients_data;
+};
+
+void *srv_main_new_client(struct srv_net_client *client, void *main_data);
+void srv_main_placement_received(struct srv_net_client *client,
+		void *client_data, void *main_data);
+void srv_main_del_client(struct srv_net_client *client, void *client_data,
+		void *main_data);
+void srv_main_move_received(struct srv_net_client *client, void *client_data,
+		void *main_data);
+void srv_main_nick_received(struct srv_net_client *client, void *client_data,
+		void *main_data);
+
+#endif // BATTLESHIP_SRV_MAIN_H_
--- server/srv_net.h
+++ server/srv_net.h
@@ -1,1 +1,17 @@
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 smartindent: */
+#ifndef BATTLESHIP_SRV_NET_H_
+#define BATTLESHIP_SRV_NET_H_
+
+struct srv_net_client_ops {
+	void *new_client(struct srv_net_client *client, void *main_data);
+	void placement_received(struct srv_net_client *client, void *client_data,
+			void *main_data);
+	void del_client(struct srv_net_client *client, void *client_data,
+			void *main_data);
+	void move_received(struct srv_net_client *client, void *client_data,
+			void *main_data);
+	void nick_received(struct srv_net_client *client, void *client_data,
+			void *main_data);
+};
+
+#endif // BATTLESHIP_SRV_NET_H_
