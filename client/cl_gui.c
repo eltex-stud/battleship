e96cc21cc1a4d249eea4e214bc662aa5bd839abe
Iljichev <yailjichev@yandex.ru>
2014-07-21 15:19:40 +0700

    Added funciton giu_start; Added struct gui;

--- client/cl_gui.c
+++ client/cl_gui.c
@@ -1,5 +1,46 @@
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 smartindent: */
+#include "cl_gui.h"
 
 
+void *cl_gui_key_processing(void *arg)
+{
+	struct gui *options;
 
+	options = (struct gui*)arg;
+
+}
+
+struct gui *gui_start(struct main_queue *main_queue_h)
+{
+	struct gui *options;
+	pthread_t gui_pthread;
+	int idx, jdx;
+
+	initscr();
+
+	if(getmaxx(stdscr) < 94 || getmaxy(stdscr) < 39) {
+		endwin();
+		printf("ERROR::Low Resolution: Resolution at least must be 94x39\n");
+		return NULL;
+	}
+
+	start_color();
+
+	for(idx = 1; idx < 9; idx++) {
+		for(jdx = 1; jdx < 9; jdx++) {
+			init_pair( ((jdx * 10) + idx), jdx, idx);
+		}
+	}
+
+	options = (struct gui *) malloc(sizeof(struct gui));
+
+	options->bg_color = 1;
+	options->font_color = 8;
+	options->state = 0;
+	options->main_queue_head = main_queue_h;
+
+	pthread_create(&gui_pthread, NULL, cl_gui_key_processing, (void *) options);
+
+	return options;
+}
 
--- client/cl_gui.h
+++ client/cl_gui.h
@@ -1,2 +1,37 @@
-
 /* vim: set noexpandtab tabstop=4 shiftwidth=4 smartindent: */
+#ifndef BATTLESHIP_CLIENT_GUI_H_
+#define BATTLESHIP_CLIENT_GUI_H_
+
+#include "cl_main.c"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <curses.h>
+#include <pthread.h>
+
+
+#define CUR_COLOR ((bg_color * 10) + font_color)
+
+void *cl_gui_key_processing(void *arg);
+
+struct gui *gui_start(struct main_queue *main_queue_h);
+
+int gui_input_nick(struct gui *options);
+
+int gui_main_window(struct gui *options, char map[10][10]);
+
+struct gui
+{
+	WINDOW *nick_window;
+	WINDOW *my_map;
+	WINDOW *enemy_map;
+	WINDOW *chat;
+	WINDOW *line_stat;
+	unsigned short bg_color;
+	unsigned short font_color;
+	unsigned short state;
+	struct main_queue *main_queue_head;
+};
+
+#endif // BATTLESHIP_CLIENT_GUI_H_
