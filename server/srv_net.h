cff6f08d69cc543f41e731b58607e5a589478ebe
Aleksandr Yurev <n/a>
2014-07-21 06:33:29 +0400

    Change srv_net.h 	Add protope functions

--- server/srv_net.h
+++ server/srv_net.h
@@ -14,4 +14,17 @@ struct srv_net_client_ops {
 			void *client_data, void *main_data);
 };
 
+int srv_net_start(char ip, short int port);
+int srv_net_stop(int net);
+
+void srv_net_wait_events(int net, int *clients[],
+		struct srv_net_client_ops client ops, *main_data);
+
+int srv_net_send_shot_result(int client, struct shot, enum result r);
+int srv_net_send_game_start(int client, int turn);
+int srv_net_send_placement(int client, char **placement);
+int srv_net_send_game_end(int client, enum result r);
+int srv_net_send_error(int client, enum error_msg er_msg);
+
+int srv_net_del_client(int client);
 #endif // BATTLESHIP_SRV_NET_H_
